9287f6e7266a7584dd50fe706105380121cb6b8c
hitesh bhatia <hiteshbhatiya787@gmail.com>
2022-03-31 19:39:03 +0530

    Intial author routes

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ const app = express();
 const mongoose = require("mongoose");
 const expressLayout = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
+
 app.use(expressLayout);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 app.use("/", indexRouter);
-
+app.use("/authors", authorRouter);
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
